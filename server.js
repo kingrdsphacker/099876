8e8dbe77d7628d5fed7b86d10ffdcb2d15ad2685
Glitch (loookoa) <none>
2020-08-04 08:03:22 +0000

    👨‍👨‍👦🔦 Checkpoint ./server.js:6310492/853

--- server.js
+++ server.js
@@ -4533,28 +4533,3 @@ client.on('message' , message => {//جميع حقوق البوت محفوضة ل
 
 
 
-// Create an event listener for new guild members
-client.on('guildMemberAdd', member => {
-  // Send the message to a designated channel on a server:
-  const channel = member.guild.channels.cache.find(ch => ch.name === 'member-log');
-  // Do nothing if the channel wasn't found on this server
-  if (!channel) return;
-  // Send the message, mentioning the member
-  channel.send(`  Welcome to the server, ${member}  `);
-});
-
-
-
-
-
-// Create an event listener for new guild members
-client.on('guildMemberAdd', inviter => {
-  // Send the message to a designated channel on a server:
-  const channel = inviter.guild.channels.cache.find(ch => ch.name === 'inviter-log');
-  // Do nothing if the channel wasn't found on this server
-  if (!channel) return;
-  // Send the message, mentioning the member
-  channel.send(`  Welcome to the server, ${inviter}  `);
-});
-
-
