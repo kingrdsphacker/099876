6499e381b707d377170804086c16eee04557746b
Glitch (loookoa) <none>
2020-08-21 17:42:04 +0000

    ⛹🍈 Checkpoint ./server.js:6310492/368

--- server.js
+++ server.js
@@ -2,17 +2,25 @@
 const http = require("http");
 const express = require("express");
 const app = express();
-app.get("/", (request, response) => {
+
+
+app.use(express.static('public'));
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+var listener = app.listen(process.env.PORT, function() {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+/*app.get("/", (request, response) => {
   response.sendStatus(200);
 });
 app.listen(process.env.PORT);
 setInterval(() => {
   http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
 }, 280000);
+*/
 
 
-/*
-var express = require('express');
 /*
 var app = express();
 app.use(express.static('public'));
