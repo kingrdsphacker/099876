392bd7b71c7ec4d48c45c9a26a4ffe15b071189d
Glitch (kinghac) <none>
2020-09-07 11:39:25 +0000

    🎽💳 Checkpoint ./package.json:6310492/112 ./bot.js:6310492/15715

---
+++ bot.js
@@ -0,0 +1,453 @@
+// server.js
+// where your node app starts
+
+// we've started you off with Express (https://expressjs.com/)
+// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
+const express = require("express");
+const http = require("http");
+const app = express();
+app.get("/", (request, response) => {
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000)
+const { Client, RichEmbed } = require("discord.js");
+var { Util } = require("discord.js");
+const { prefix, devs } = require("./config");
+const wait = require("util").promisify(setTimeout);
+
+const client = new Client({ disableEveryone: true });
+const ytdl = require("ytdl-core");
+const canvas = require("canvas");
+const convert = require("hh-mm-ss");
+const fetchVideoInfo = require("youtube-info");
+const botversion = require("./package.json").version;
+const simpleytapi = require("simple-youtube-api");
+const moment = require("moment");
+const fs = require("fs");
+const util = require("util");
+const gif = require("gif-search");
+const opus = require("node-opus");
+const ms = require("ms");
+const jimp = require("jimp");
+const { get } = require("snekfetch");
+const guild = require("guild");
+const dateFormat = require("dateformat");
+const YouTube = require("simple-youtube-api");
+const youtube = new YouTube("AIzaSyB1QqsuVEl7EnfkAKVz5Lv6Gn20rKQeQko"); //تعديل اساسي سوي اي بي اي جديد
+const hastebins = require("hastebin-gen");
+const getYoutubeID = require("get-youtube-id");
+
+client.login(process.env.BOT_TOKEN);
+const yt_api_key = "AIzaSyB1QqsuVEl7EnfkAKVz5Lv6Gn20rKQeQko"; ///تعديل اساسي سوي اي بي اي جديد
+;
+
+var table = require("table").table;
+const Discord = require("discord.js");
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+//كود تغيير الحالة
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log(client.guilds.map(c => `${c.name} : ${c.me.hasPermission(8)}`));
+  client.user.setStatus("online");
+});
+
+
+const invites = {};
+
+client.on('ready', () => {
+  wait(1000);
+  client.guilds.forEach(king => {
+    king.fetchInvites().then(guildInvites => {
+      invites[king.id] = guildInvites;
+    });
+  });
+});
+
+client.on('guildMemberAdd', member => {
+  member.guild.fetchInvites().then(guildInvites => {
+    const gamer = invites[member.guild.id];
+    invites[member.guild.id] = guildInvites;
+    const invite = guildInvites.find(i => gamer.get(i.code).uses < i.uses);
+    const inviter = client.users.get(invite.inviter.id);
+    const welcome = member.guild.channels.find(channel => channel.name === "𝑾𝒆𝒍𝒄𝒐𝒎𝒆☃");
+    welcome.send(`** ${member} joined  invited by ${inviter}   (  ${invite.uses} invites ) ** `)
+  });
+});
+
+
+//////////////////////////////////////////////////////
+  client.on('message',message =>{
+ if(message.content.split(' ')[0].toLowerCase() == prefix + 'invites') {
+let guild = message.guild
+var codes = [""]
+ var nul = 0
+      
+guild.fetchInvites()
+.then(invites => {
+invites.forEach(invite => {
+if (invite.inviter === message.author) {
+    nul+=invite.uses
+codes.push(`discord.gg/${invite.code}`)
+}
+ 
+})
+  if (nul > 0) {
+      const e = new Discord.RichEmbed()
+      .addField(`${message.author.username}`, `لقد قمت بدعوة **${nul}** شخص`)
+      .setColor('#36393e')
+      message.channel.send(e)
+  }else {
+                       var embed = new Discord.RichEmbed()
+                        .setColor("#000000")
+                        .addField(`${message.author.username}`, `لم تقم بدعوة أي شخص لهذة السيرفر`)
+
+                       message.channel.send({ embed: embed });
+                        return;
+                    }
+}).then(m => {
+if (codes.length < 0) {
+    var embed = new Discord.RichEmbed()
+.setColor("#000000")
+.addField(`Your invite codes in ${message.guild.name}`, `You currently don't have any active invites! Please create an invite and start inviting, then you will be able to see your codes here!`)
+message.channel.send({ embed: embed });
+return;
+} else {
+    let embed = new Discord.RichEmbed()
+.setColor("#000000")
+.addField(`ᵛᵉʳⁱᶠⁱᵉᵈ  
+Your invite codes in ${message.guild.name}`, ` Invite Codes :\n${codes.join("\n")}`)
+.setColor('#36393e')
+message.channel.send({ embed: embed });
+return;
+}
+})
+}
+
+});
+
+
+
+
+client.on('message', message => {
+
+    if(message.content === "قفل") {
+                        if(!message.channel.guild) return message.reply('** This command only for servers ❌ **');
+
+if(!message.member.hasPermission('MANAGE_CHANNELS')) return message.reply
+           message.channel.overwritePermissions(message.guild.id, {
+         SEND_MESSAGES: false
+
+           }).then(() => {
+               message.reply("** ✖️ | امر وطاعه يامولاي الشات مغلق  **")
+           });
+             }
+
+ if(message.content === "فتح") {
+                     if(!message.channel.guild) return message.reply('** This command only for servers ❌ **');
+
+if(!message.member.hasPermission('MANAGE_CHANNELS')) return message.reply
+           message.channel.overwritePermissions(message.guild.id, {
+         SEND_MESSAGES: true
+
+           }).then(() => {
+               message.reply("**べ | امر وطاعه يامولاي تم فتح الشات **")
+           });
+             }
+             
+      
+    
+});
+
+
+client.on('guildMemberUpdate', (ninja, nano,) => {//ninja%nano
+if(ninja.roles.size < nano.roles.size) {
+ let role = nano.roles.filter(r => !ninja.roles.has(r.id)).first();//ninja%nano
+            let embed = new Discord.RichEmbed()
+            .setThumbnail(ninja.guild.iconURL)//ninja%nano
+            .setColor('RANDOM')
+            .setDescription(`
+**New Role**
+ 
+**✨ Role Name:** ( ${role.name} )
+ 
+**🔗 Server:** ${nano.guild.name}`)//ninja%nano
+            .setTimestamp()
+           .setFooter(`🔰 Guild ID : ${ninja.guild.id}`) 
+            nano.user.send(embed); 
+}
+});
+
+
+
+const temp = JSON.parse(fs.readFileSync('./temp.json', 'utf8'));
+client.on('message', async message => {
+ if(message.channel.type === "dm") return;
+  if(message.author.bot) return;
+   if(!temp[message.guild.id]) temp[message.guild.id] = {
+    time: "3000",
+     category : 'click here',
+      channel : 'click here'
+       }
+        if(message.content.startsWith('=temp on')){
+         if(!message.member.hasPermission(`MANAGE_GUILD`)) return;
+          var ggg= message.guild.createChannel('click here', 'category').then(cg => {
+           var ccc =message.guild.createChannel('click here', 'voice').then(ch => {
+            ch.setParent(cg)
+             message.channel.send('**Done ,**')
+              client.on('message' , message => {
+               if(message.content === '=temp off') {
+                if(!message.member.hasPermission(`MANAGE_GUILD`)) return;
+                 cg.delete()
+                  ch.delete()
+                   message.channel.send('**Done ,**')
+                    }
+                     });
+                      const time = temp[message.guild.id].time
+                       client.on('message' , message => {
+                        if (message.content.startsWith(prefix + "temptime")) {
+                         if(!message.member.hasPermission(`MANAGE_GUILD`)) return;
+                          let newTime= message.content.split(' ').slice(1).join(" ")
+                          if(!newTime) return message.reply(`**${prefix}temptime <time>  \`1000 = 1s\`**`)
+	                 if(isNaN(newTime)) return message.reply(`** The Time Be Nambers :face_palm: **`);
+	                if(newTime < 1) return message.reply(`**The Time Be Up \`3000s\`**`)
+                       temp[message.guild.id].time = newTime
+                      message.channel.send(`**Temp Rooms Time Change To \`${newTime}\`**`);
+                     }
+                    });
+                   client.on('voiceStateUpdate', (old, neww) => {
+                  let newUserChannel = neww.voiceChannel
+                 let oldUserChannel = old.voiceChannel
+                temp[message.guild.id].category = cg.id
+               temp[message.guild.id].channel = ch.id
+              let channel = temp[message.guild.id].channel
+             let category = temp[message.guild.id].category
+            if(oldUserChannel === undefined && newUserChannel !== undefined && newUserChannel.id == channel) {
+           neww.guild.createChannel(neww.displayName , 'voice').then(c => {
+          c.setParent(category)
+         let scan = setTimeout(()=>{
+        if(!neww.voiceChannel) {
+       c.delete();
+      client.channels.get(channel).overwritePermissions(neww, {
+     CONNECT:true,
+    SPEAK:true
+   })
+  }
+ }, temp[neww.guild.id].time);
+  c.overwritePermissions(neww, {
+   CONNECT:true,
+    SPEAK:true,
+     MANAGE_CHANNEL:true,
+      MUTE_MEMBERS:true,
+       DEAFEN_MEMBERS:true,
+	MOVE_MEMBERS:true,
+	 VIEW_CHANNEL:true
+	  })
+	   neww.setVoiceChannel(c)
+            })
+             client.channels.get(channel).overwritePermissions(neww, {
+	      CONNECT:false,
+	       SPEAK:false
+		})
+               }
+              })
+             })
+           })
+          }
+         fs.writeFile("./temp.json", JSON.stringify(temp), (err) => {
+        if(err) console.error(err)
+       })
+      });
+  // const wlcImage = await fs.readFile("./welcome111.png"); //اسم الصورة
+
+///////////////////////////////////////////////////////////////////
+
+/*
+const welcome = JSON.parse(fs.readFileSync("./welcomer.json", "utf8")); //ملف تخزين كود الويلكم
+
+
+//تحديد روم الويلكم
+client.on("ready", async () => {
+  wait(1000);
+
+  await client.guilds.forEach(g => {
+    g.fetchInvites().then(guildInvites => {
+      invites[g.id] = guildInvites;
+    });
+  });
+});
+var gg2;
+
+client.on("guildMemberAdd", async member => {
+  if (!welcome[member.guild.id])
+    welcome[member.guild.id] = {
+      by: "off",
+      channel: null
+    };
+
+  if (welcome[member.guild.id].by === "off") return;
+  let channel = member.guild.channels.find(
+    c => c.name == welcome[member.guild.id].channel
+  );
+  if (!channel) return;
+
+  await member.guild.fetchInvites().then(async guildInvites => {
+    const ei = await invites[member.guild.id];
+    invites[member.guild.id] = guildInvites;
+    const invite = await guildInvites.find(i => ei.get(i.code).uses < i.uses);
+    const inviter1 = await invite.inviter;
+    const inviter =
+      (await client.users.get(invite.inviter.id)) ||
+      client.users.get(member.guild.owner.user.id);
+    const logChannel = member.guild.channels.find(
+      channel => channel.name === `${welcome[member.guild.id].channel}`
+    );
+    if (!logChannel) return console.log("I can't find welcomeChannel");
+    let gg1 = await welcome[member.guild.id].msg.replace(
+      "[member]",
+      `<@!${member.id}>`
+    );
+    if (!inviter1 || !inviter1.id) {
+      gg2 = await gg1.replace("[inviter]", `<@${member.guild.ownerID}>`);
+    } else {
+      gg2 = await gg1.replace("[inviter]", `<@${inviter1.id}>`);
+    }
+    setTimeout(() => {
+      logChannel.send(`${gg2}`);
+    }, 2000);
+    fs.writeFile("./welcome.json", JSON.stringify(welcome), err => {
+      if (err)
+        console.error(err).catch(err => {
+          console.error(err);
+        });
+    });
+  });
+});
+
+client.on("message", async message => {
+  if (!message.channel.guild) return;
+  let room = message.content.split(" ").slice(1);
+  let findroom = message.guild.channels.find(r => r.name == room);
+  if (message.content.startsWith(prefix + "setWelcomer")) {
+    if (!welcome[message.guild.id]) {
+      if (!message.channel.guild)
+        return message.reply("**This Command Only For Servers**");
+      if (!message.member.hasPermission("MANAGE_GUILD"))
+        return message.channel.send(
+          "**Sorry But You Dont Have Permission** `MANAGE_GUILD`"
+        );
+      if (!room) return message.channel.send("Please Type The Channel Name");
+      if (!findroom) return message.channel.send("Cant Find This Channel");
+      let embed = new Discord.RichEmbed()
+        .setTitle("**Done The Welcome Has Been Setup**")
+        .addField("Channel:", `${room}`)
+        .addField("Requested By:", `${message.author}`)
+        .addField(
+          "Default Message:",
+          `**Welcome [member], You Joined by [inviter] invite**`
+        )
+        .setThumbnail(message.author.avatarURL)
+        .setFooter(`${client.user.username}`);
+      message.channel.sendEmbed(embed);
+      welcome[message.guild.id] = {
+        channel: room,
+        onoff: "On",
+        by: "On",
+        msg: `**Welcome [member], You Joined by [inviter] invite**`
+      };
+      fs.writeFile("./welcomer.json", JSON.stringify(welcome), err => {
+        if (err) console.error(err);
+      });
+    } else if (welcome[message.guild.id].channel) {
+      let msg = await welcome[message.guild.id].msg;
+      let by = await welcome[message.guild.id].by;
+      if (!message.channel.guild)
+        return message.reply("**This Command Only For Servers**");
+      if (!message.member.hasPermission("MANAGE_GUILD"))
+        return message.channel.send(
+          "**Sorry But You Dont Have Permission** `MANAGE_GUILD`"
+        );
+      if (!room) return message.channel.send("Please Type The Channel Name");
+      if (!findroom) return message.channel.send("Cant Find This Channel");
+      let embed = new Discord.RichEmbed()
+        .setTitle("**Done The Welcome Has Been Setup**")
+        .addField("Channel:", `${room}`)
+        .addField("Requested By:", `${message.author}`)
+        .addField("Default Message:", msg)
+        .setThumbnail(message.author.avatarURL)
+        .setFooter(`${client.user.username}`);
+      message.channel.sendEmbed(embed);
+      welcome[message.guild.id] = {
+        channel: room,
+        onoff: "On",
+        by: by,
+        msg: msg
+      };
+      fs.writeFile("./welcomer.json", JSON.stringify(welcome), err => {
+        if (err) console.error(err);
+      });
+    }
+  }
+});
+
+client.on("message", async message => {
+  let messageArray = message.content.split(" ");
+  if (message.content.startsWith(prefix + "setMessage")) {
+    if (!welcome[message.guild.id] || !welcome[message.guild.id].onoff == "On")
+      return message.channel.send(
+        `**please type \`${prefix}setWelcomer\` first **`
+      );
+    let filter = m => m.author.id === message.author.id;
+    let thisMessage;
+    let thisFalse;
+    let room = welcome[message.guild.id].channel;
+    if (!message.member.hasPermission("MANAGE_GUILD"))
+      return message.channel.send("You don't have permission").then(msg => {
+        msg.delete(4500);
+        message.delete(4500);
+      });
+
+    message.channel
+      .send(
+        `**من فضلك اكتب رسالة الترحيب الان:
+لعمل منشن للعضو او الشخص الذى قام بدعوتة
+Ex : 
+\`[member] Joined the server by [inviter]\`**`
+      )
+      .then(msg => {
+        message.channel
+          .awaitMessages(filter, {
+            max: 1,
+            time: 90000,
+            errors: ["time"]
+          })
+          .then(collected => {
+            collected.first().delete();
+            thisMessage = collected.first().content;
+            msg.edit("**تم الاعداد بنجاح**").then(msg => {
+              let embed = new Discord.RichEmbed()
+                .setTitle("**Done The Welcome Msg Has Been Setup**")
+                .addField("Message:", `${thisMessage}`)
+                .setThumbnail(message.author.avatarURL)
+                .setFooter(`${client.user.username}`);
+              message.channel.sendEmbed(embed);
+              welcome[message.guild.id] = {
+                channel: room,
+                onoff: "On",
+                by: "On",
+                msg: thisMessage
+              };
+              fs.writeFile("./welcomer.json", JSON.stringify(welcome), err => {
+                if (err) console.error(err);
+              });
+            });
+          });
+      });
+  }
+});
+*/
