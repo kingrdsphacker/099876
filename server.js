8504ebe7f9a4f9541b0e1ee5b1c7d4ad84d3ee59
Glitch (kinghac) <none>
2020-09-13 19:14:48 +0000

    🎹👩‍❤️‍💋‍👩 Checkpoint ./yt.js:6310492/260 ./server.js:6310492/1162

--- server.js
+++ server.js
@@ -91,31 +91,8 @@ client.on("message", message => {
 });
 
 
-client.db = require("quick.db");
-client.request = new (require("rss-parser"))();
-client.config = require("./yt.js");
 
-function handleUploads() {
-    if (client.db.fetch(`postedVideos`) === null) client.db.set(`postedVideos`, []);
-    setInterval(() => {
-        client.request.parseURL(`https://www.youtube.com/feeds/videos.xml?channel_id=${client.yt.channel_id}`)
-        .then(data => {
-            if (client.db.fetch(`postedVideos`).includes(data.items[0].link)) return;
-            else {
-                client.db.set(`videoData`, data.items[0]);
-                client.db.push("postedVideos", data.items[0].link);
-                let parsed = client.db.fetch(`videoData`);
-                let channel = client.channels.cache.get(client.yt.channel);
-                if (!channel) return;
-                let message = client.yt.messageTemplate
-                    .replace(/{author}/g, parsed.author)
-                    .replace(/{title}/g, Discord.Util.escapeMarkdown(parsed.title))
-                    .replace(/{url}/g, parsed.link);
-                channel.send(message);
-            }
-        });
-    }, client.yt.watchInterval);
-}
+
 
 ////كود تيكت
 client.on("message", message => {
--- yt.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    channel: "706268349562224690",
-    messageTemplate: "Hello @everyone, **{author}** just now uploaded a video **{title}**!\n{url}",
-    channel_id: "UCCiClXBQwCvqt-onlKr5URw",
-    watchInterval: 30000
-}
