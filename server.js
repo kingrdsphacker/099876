b50db837648440f47be6addba5ba711f242f1cab
Glitch (kinghac) <none>
2020-09-19 00:10:30 +0000

    🦁🚞 Checkpoint ./server.js:6310492/1029

--- server.js
+++ server.js
@@ -4865,29 +4865,3 @@ client.on("message", message => {
     message.react("??");
   }
 });
-
-const cookies = require('cookiesdb');
-
-client.on('message', (message) => { // We use this, for when a message is ran.
-    if (message.author.bot || message.channel.type !== "text" || message.author === client.user) return; // If the autor is a bot, or the channel is not in guild, or the author is the bot, return.
-
-    cookies.fetchCookies(`guildConf_${message.guild.id}`).then(c => {
-        var prefix = c.text;
-        
-        if (!prefix) { // If c.text a.k.a prefix doesn't exist, prefix will be "!"
-            prefix = "!";
-        }
-
-        var args = message.content.split(' ').slice(1);
-        var cmd = message.content.split(' ')[0].replace(prefix, '');
-
-        if (cmd === "ping") {
-            message.channel.send('pong!')
-        } else
-        if (cmd === "setprefix") {
-            cookies.updateText(`guildConf_${message.guild.id}`, args.join(' ')).then(p => { // Sets the custom prefix
-                message.channel.send(`Changed the Guild prefix to **${p.text}**`)
-            });
-        }
-    })
-});
